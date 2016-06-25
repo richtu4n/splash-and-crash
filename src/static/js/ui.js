6e21b92c788860c11584650831c5321b3c3642ab
Rich Turnbull <rpturnbull1@gmail.com>
2016-06-26 00:46:43 +0100

    implemented functionality ui for form and lightbox

--- src/static/js/ui.js
+++ src/static/js/ui.js
@@ -1,5 +1,60 @@
 $(document).ready(function(){
 
-	//$('.payment-form').animate({marginBottom: "0"}, 150);
-
+	stripe.init();
+	lightBox.init();
 });
+
+var stripe = {
+
+	state: {
+		open: false
+	},
+	element: null,
+	init: function(){
+		this.element = $('.payment-form');
+	},
+	toggle: function(){
+		if(this.state.open){
+			this._hide();
+		} else {
+			this._show();
+		}
+	},
+	_show: function(){
+		this.element.animate({ marginBottom: '0' }, 150);
+		lightBox._show();
+		this.state.open = true;
+	},
+	_hide: function(){
+		this.element.animate({ marginBottom: '-100%' }, 150);
+		lightBox._hide();
+		this.state.open = false;
+	}
+};
+
+
+var lightBox = {
+
+	state: {
+		open: false
+	},
+	element: null,
+	init: function(){
+		this.element = $('.light-box');
+	},
+	toggle: function(){
+		if(this.state.open){
+			this._hide();
+		} else {
+			this._show();
+		}
+	},
+	_show: function(){
+		this.element.css('z-index', '1');
+		this.state.open = true;
+	},
+	_hide: function(){
+		this.element.css('z-index', '-1');
+		this.state.open = false;
+	}
+};
