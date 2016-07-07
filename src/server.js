eff032a073490291a1b9918ac33704ccb27b9a36
Pete Turnbull <pete.j.turnbull@gmail.com>
2016-07-07 22:11:51 +0100

    Setup enforce ssl

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
-var async      = require('asyncawait/async');
-var await      = require('asyncawait/await');
-var koa        = require('koa');
-var koaBody    = require('koa-body');
-var router     = require('koa-router')();
-var hbs        = require('koa-hbs');
-var serve      = require('koa-static-server');
-var Promise    = require('bluebird');
-var co         = require('co');
-var log        = require('./utilities/logger');
-var koaLogger  = require('./utilities/koa-logger');
-var routes     = require('./routes');
-var pay        = require('./utilities/payments');
-var config     = require('./config/config');
-var mongo      = require('./mongo/connection');
+var async        = require('asyncawait/async');
+var await        = require('asyncawait/await');
+var koa          = require('koa');
+var koaBody      = require('koa-body');
+var router       = require('koa-router')();
+var hbs          = require('koa-hbs');
+var serve        = require('koa-static-server');
+var Promise      = require('bluebird');
+var co           = require('co');
+var log          = require('./utilities/logger');
+var koaLogger    = require('./utilities/koa-logger');
+var routes       = require('./routes');
+var pay          = require('./utilities/payments');
+var config       = require('./config/config');
+var mongo        = require('./mongo/connection');
+var enforceHttps = require('koa-sslify');
 
 //exports
 var app = koa();
@@ -26,6 +27,9 @@ router.post('/pay', routes.pay);
 
 //middleware
 app.use(koaLogger());
+app.use(enforceHttps({
+  trustProtoHeader: true
+}));
 app.use(koaBody({ formidable: { uploadDir: __dirname } }));
 app.use(hbs.middleware({viewPath: __dirname + '/views'}));
 app.use(serve({rootDir: __dirname + "/static", rootPath: "/static"}));
