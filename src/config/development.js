c73eec1a83d34b3142c53b419ffbc3b54d60d917
Rich Turnbull <rpturnbull1@gmail.com>
2016-09-06 20:31:56 +0100

    updated config

--- src/config/development.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    logger: {
-        enabled: ['debug', 'info', 'warn', 'error']
-    },
-    port: 3030,
-    accessKeyId: 'AKIAIMOL22VCKBGCBYGA',
-    secretAccessKey: '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',
-    region: 'eu-west-1'
-};
